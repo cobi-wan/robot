3de508748639025b58017da6128df1ae663a7ced
Benjamin Carnicelli <benjamin.carnicelli@hppi.com>
2022-05-24 15:48:17 -0600

    Mapping and graph data structure completed

---
+++ Classes.py
@@ -0,0 +1,179 @@
+from re import X
+import cv2 as cv
+import time
+from cv2 import MARKER_SQUARE
+import numpy as np
+
+
+## Robot Class Definitions ##
+class partRunner:
+    numBots = 0
+    path = []
+    path.append(1) # Assume the bot always has to go to the first node first
+    length = 14 # Dimensions of bot in inches
+    width = 11
+
+    
+    def __init__(self, x, y, t):
+        self.botIndex = self.numBots
+        partRunner.numBots += 1
+        self.xCord = x
+        self.yCord = y
+        self.tCord = t
+
+    def add(self, point):
+        self.path.append(point)
+
+class cart:
+    numBots = 0
+    path = []
+    path.append(1) # Assume the bot always has to go to the first node first
+    length = 36 # Dimensions of bot in inches
+    width = 24
+
+    
+    def __init__(self, x, y, t):
+        self.botIndex = self.numBots
+        cart.numBots += 1
+        self.xCord = x
+        self.yCord = y
+        self.tCord = t
+
+    def add(self, point):
+        self.path.append(point)
+
+
+
+
+
+
+
+
+
+
+## MAP Class Definition ## 
+class MAP:
+    mapScale = 0.1 # 1 px = 1 mm
+    worldScale = 10
+    width = int(1152*worldScale*mapScale)
+    height = int(648*worldScale*mapScale)
+    dimensions = (width, height)
+    botList = np.array([])
+
+    def __init__(self, img, bots, graph):
+        self.UI = cv.imread(img)
+        self.UI = cv.resize(self.UI, self.dimensions, interpolation = cv.INTER_LINEAR)
+        self.botList = bots
+        self.network = graph
+
+    def robot2World(self, cords, botNum): 
+        t = self.botList[botNum].tCord
+        x = self.botList[botNum].xCord + cords[0]*np.cos(t) - cords[1]*np.sin(t)
+        y = self.botList[botNum].yCord + cords[0]*np.sin(t) - cords[1]*np.cos(t)
+
+        return [x, y]
+
+    def robotPoints(self, botNum):
+        bot = self.botList[botNum]
+        L = bot.length * 2.54 * 2 # Parameters to be adjusted for robot size and camera resolution
+        W = bot.width *2.54 * 2
+
+        w1 = self.robot2World((-W/2, L/2), botNum)
+        w2 = self.robot2World((W/2, L/2), botNum)
+        w3 = self.robot2World((W/2, -L/2), botNum)
+        w4 = self.robot2World((-W/2, -L/2), botNum)
+    
+        p1 = (int(w1[0]*self.mapScale), self.height - int(w1[1]*self.mapScale))
+        p2 = (int(w2[0]*self.mapScale), self.height - int(w2[1]*self.mapScale))
+        p3 = (int(w3[0]*self.mapScale), self.height - int(w3[1]*self.mapScale))
+        p4 = (int(w4[0]*self.mapScale), self.height - int(w4[1]*self.mapScale))
+
+        pts = np.array([p1, p2, p3, p4])
+        return pts
+
+
+    def updateBotPos(self):
+        tempMap = self.UI.copy() 
+        for i in self.botList:
+            pts = self.robotPoints(i.botIndex)
+            cv.drawContours(tempMap, [pts], 0, (255, 0, 0), 1)
+        cv.imshow('Map', tempMap)
+
+    def drawNodes(self):
+        for i in self.network.nodes:
+            cv.drawMarker(self.UI, (int(i.x*self.mapScale), int(i.y*self.mapScale)), (255, 0, 0), MARKER_SQUARE, 6)
+        cv.imshow('Map', self.UI)
+
+    def drawPaths(self):
+        for i in self.network.edges:
+            cv.line(self.UI, (int(i.n1.x*self.mapScale), int(i.n1.y*self.mapScale)), (int(i.n2.x*self.mapScale), int(i.n2.y*self.mapScale)), (0, 0, 0), 2)
+        cv.imshow('Map', self.UI)
+        cv.waitKey()
+
+
+
+
+
+
+
+
+
+
+## Graph Definitions ## 
+class edge:
+
+    def length(self):
+        return np.sqrt((self.n1.x - self.n2.x)**2 + (self.n1.y - self.n2.y)**2)
+
+    def __init__(self, node1, node2):
+        self.n1 = node1
+        self.n2 = node2
+        self.len = self.length()
+
+class node:
+    edges = []
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class graph:
+    def __init__(self, nodes = None, edges = None):
+        if nodes is None:
+            nodes = []
+        self.nodes = nodes
+        if edges is None:
+            edges = []
+        self.edges = edges
+    
+    def addNode(self, node):
+        self.nodes.append(node)
+
+    def addEdge(self, node1, node2):
+        pass
+# class paths:
+#     def __init__(self, gdict=None):
+#         if gdict is None:
+#             gdict = {}
+#         self.gdict = gdict
+
+#     def edges(self):
+#         return self.findEdges()
+    
+#     def addEdge(self, edge):
+#         edge = set(edge)
+#         (vrtx1, vrtx2) = tuple(edge)
+#         if vrtx1 in self.gdict:
+#             self.gdict[vrtx1].append(vrtx2)
+#         else: 
+#             self.gdict[vrtx1] = [vrtx2]
+
+#     def findEdges(self):
+#         edgeName = []
+#         for vrtx in self.gdict:
+#             for nxtvrtx in self.gdict[vrtx]:
+#                 if {nxtvrtx, vrtx} not in edgeName:
+#                     edgeName.append({vrtx, nxtvrtx})
+#         return edgeName
+
+
--- MapData.py
+++ MapData.py
@@ -1,14 +1,12 @@
 import cv2 as cv
 import time
 import numpy as np
-
-mapScale = 0.1 # 1 px = 1 mm
-worldScale = 10
-MAP = cv.imread("lib\Map.png") # Read in picture of the map. Will look into reading different files later
-width = int(1152*worldScale*mapScale)
-height = int(648*worldScale*mapScale)
-dimensions = (width, height)
-MAP = cv.resize(MAP, dimensions, interpolation = cv.INTER_LINEAR)
+from Classes import partRunner
+from Classes import cart
+from Classes import MAP
+from Classes import edge
+from Classes import node
+from Classes import graph
 
 # Robot path points
 p1 = (int(1), int(1))
@@ -20,41 +18,39 @@ posRobx = 1000
 posRoby = 1000
 posRobt = 0
 
-
-def robot2World(cords):
-    x = posRobx + cords[0]*np.cos(posRobt) - cords[1]*np.sin(posRobt)
-    y = posRoby + cords[0]*np.sin(posRobt) - cords[1]*np.cos(posRobt)
-
-    return [x, y]
-
-def robotTriangle():
-    L = 14*2.54 * 5
-    W = 11*2.54 * 5
-
-    p1 = (-W/2, L/2)
-    p2 = (W/2, L/2)
-    p3 = (W/2, -L/2)
-    p4 = (-W/2, -L/2)
-
-    w1 = robot2World(p1)
-    w2 = robot2World(p2)
-    w3 = robot2World(p3)
-    w4 = robot2World(p4)
-
-    pts = np.array([(int(w1[0]*mapScale),height - int(w1[1]*mapScale)),(int(w2[0]*mapScale),height - int(w2[1]*mapScale)),(int(w3[0]*mapScale),height - int(w3[1]*mapScale)),(int(w4[0]*mapScale),height - int(w4[1]*mapScale))])
-    #pts = np.array([int(w1[0]*mapScale, height - int(w1[1]*mapScale)), (int(w2[0]*mapScale), height - int(w2[1]*mapScale)), (int(w3[0]*mapScale), height - int(w3[1]*mapScale))])
-    return pts
-
-def updateMap():
-    map = MAP.copy()
-    pts = robotTriangle()
-    cv.drawContours(map, [pts], 0, (255, 0, 0), 4)
+def startUp():
+    b1 = partRunner(1000, 600, 0)
+    b2 = partRunner(1000, 2000, np.pi/2)
+    bList = np.array([b1, b2])
     
-    cv.imshow('Map', map)
-    cv.waitKey(33)
-    pass
+    # n1 = np.array([5760, 3240])
+    # n2 = np.array([7000, 3240])
+    # n3 = np.array([11000, 3240])
+    # n4 = np.array([7000, 4500])
+    # n5 = np.array([11000, 4500])
+    # n6 = np.array([9000, 4500])
+    # n7 = np.array([9000, 5500])
+    # n8 = np.array([2500, 5500])
+    # n9 = np.array([2500, 3240])
+    # n10 = np.array([2500, 500])
+    # n11 = np.array([5760, 500])
+    # nodes = np.array([n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11])
+    # p1 = np.array([1, 2])
+    # p2 = np.array([1, 11, 10])
+    # p3 = np.array([1, 11])
+    # p4 = np.array([2, 3])
+    # p5 = np.array([2, 4, 6])
+    # p6 = np.array([3, 5, 6])
+    # p7 = np.array([6, 7, 8, 9])
+    # p8 = np.array([9, 10])
+    # paths = np.array([p1, p2, p3, p4, p5, p6, p7, p8])
+    nodes = [node(5760, 3240), node(7000, 3240), node(11000, 3240), node(7000, 4500)]
+    edges = [edge(nodes[0], nodes[1]), edge(nodes[1], nodes[2]), edge(nodes[1], nodes[3])]
+    g = graph(nodes, edges)
 
-def defineMap(paths):
+    return bList, g
+
+def drawPaths(paths):
     map = MAP.copy()                   
     cv.drawContours(map, [paths], 0, (255, 255, 255), 2)
     cv.imshow('Map', map)
@@ -62,16 +58,17 @@ def defineMap(paths):
     pass
 
 if __name__ == '__main__':
+    cv.destroyAllWindows()
+    bList, g = startUp()
+    Map = MAP("lib\BlankMap.png", bList, g)
     #defineMap([(p1, p2), (p2, p3)])
-    pts = np.array([p1, p2, p3])
-    # defineMap(pts)
-    updateMap()
-    print("Fuck1")
-    time.sleep(1)
-    try: 
-        for i in range(100):
-            updateMap()
-            posRobx += 10
-            posRoby += 10
-    except KeyboardInterrupt:
-        print("Darn")
+    Map.updateBotPos()
+    Map.drawNodes()
+    Map.drawPaths()
+    # try: 
+    #     for i in range(100):
+    #         updateMap(Map)
+    #         posRobx += 10
+    #         posRoby += 10
+    # except KeyboardInterrupt:
+    #     print("Darn")
