5ab4ee8244b46659ad7d9fe9bdd4ec0131982323
Benjamin Carnicelli <benjamin.carnicelli@hppi.com>
2022-06-01 07:41:10 -0600

    File organization and finishing dijkstras and path planning. Still some errors but works mostly

--- centralizedCode/MapData.py
+++ centralizedCode/MapData.py
@@ -1,3 +1,5 @@
+from cProfile import run
+from re import M
 from turtle import update
 import cv2 as cv
 from cv2 import MARKER_SQUARE
@@ -5,6 +7,7 @@ import time
 import numpy as np
 import platform
 import sys
+import csv
 from Classes import partRunner
 from Classes import cart
 from Classes import environment
@@ -13,37 +16,26 @@ from Classes import node
 from Classes import graph
 
 def startUp():
-    nodes = [node(5760, 3240), 
-            node(7000, 3240), 
-            node(11000, 3240), 
-            node(7000, 4500), 
-            node(11000, 4500), 
-            node(9000, 4500), 
-            node(9000, 5500), 
-            node(2500, 5500), 
-            node(2500, 3240),
-            node(2500, 500),
-            node(5760, 500)]
-
-    edges = [edge(nodes[0], nodes[1]),
-            edge(nodes[0], nodes[8]),
-            edge(nodes[0], nodes[10]),
-            edge(nodes[1], nodes[2]), 
-            edge(nodes[1], nodes[3]),
-            edge(nodes[2], nodes[4]),
-            edge(nodes[3], nodes[5]),
-            edge(nodes[4], nodes[5]),
-            edge(nodes[5], nodes[6]),
-            edge(nodes[6], nodes[7]),
-            edge(nodes[7], nodes[8]),
-            edge(nodes[8], nodes[9]),
-            edge(nodes[9], nodes[10])]
+    fileName = "nodeLocations.csv"
+    nodes = []
+    edges = []
+    # Read in nodes and edges from filename
+    with open(fileName, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader) # Skip the first line
+        for row in csvreader:
+            nodes.append(node(int(row[1]), int(row[2])))
+            for i in row[3:]:
+                if i != '':
+                    edges.append(edge(nodes[int(i)], nodes[int(row[0])]))
+            # print()
+        # print("Total no of rows: %d"%(csvreader.line_num))
 
     g = graph(nodes, edges)
 
-    b1 = partRunner(nodes[0].x, nodes[0].y, 0)
-    b2 = partRunner(nodes[0].x, nodes[0].y, np.pi/2)
-    b3 = partRunner(nodes[0].x, nodes[0].y, np.pi)
+    b1 = partRunner(nodes[1].x, nodes[1].y, 0)
+    b2 = partRunner(nodes[1].x, nodes[1].y, np.pi/2)
+    b3 = partRunner(nodes[1].x, nodes[1].y, np.pi)
     bList = np.array([b1, b2, b3])
 
     return bList, g
@@ -61,13 +53,13 @@ def calcSpeed(xDist, yDist, max, min):
             return spd
 
 def path(g, n1):
-    unvisited = g.nodes.copy()
+    unvisited = g.nodes.copy() # Set all nodes to unvisited
     shortestPath = {}
     previous = {}
     maxVal = sys.maxsize
-    for i in unvisited:
+    for i in unvisited: # Set the shortestPath to all nodes to the max system size
         shortestPath[i] = maxVal
-    shortestPath[n1] = 0
+    shortestPath[n1] = 0 # Shortest path to the current node is 0
 
     while unvisited: 
         currentMin = None
@@ -98,28 +90,47 @@ def getResult(previous, shortest, start, target):
     return reversed(path), shortest[target]
         
 def runDijkstras(environ, calledNodes):
-    pass
+    botNum = calledNodes[0]
+    [curr, dest] = calledNodes[1]
+    previous, shortestPath = path(environ.network, environ.network.nodes[curr])
+    path1, length = getResult(previous, shortestPath, environ.network.nodes[curr], environ.network.nodes[dest])
+    for i in path1:
+        environ.botList[botNum].add(environ.network.nodes[i.label])
+
+def getCurrLocation(environ, botNum):
+    for i in environ.network.nodes:
+        if abs(environ.botList[botNum].xCord - i.x) < 10 and abs(environ.botList[botNum].yCord - i.y) < 10:
+            return i
+    return environ.network.nodes[0]
 
 
 if __name__ == '__main__':
 
     # Initial running stuff
     cv.destroyAllWindows()
+
+    # Run startup procedure to read in nodes and create network 
     bList, g = startUp()
+
+    # Load in blank image in given folder. Allow for Windows and Mac OS
     if platform.system() == 'Windows':
         file = "ImageFiles\BlankMap.png"
     else: 
         file = "ImageFiles/BlankMap.png"
+    
+    # Create environment and draw items given in setup
     env = environment(file, bList, g)
     env.updateBotMarker() 
     env.drawPaths()
     env.drawNodes()
+    print("*******************")
+    print("Network Initialized")
+    print("*******************")
 
-    previous, shortestPath = path(env.network, env.network.nodes[0])
-    for i in range(2, 5):
-        path1, length = getResult(previous, shortestPath, env.network.nodes[0], env.network.nodes[i])
-        for j in path1:
-            env.botList[i - 2].add(env.network.nodes[j.label])
+
+    runDijkstras(env, [0, [1, 19]])
+    runDijkstras(env, [1, [1, 27]])
+    runDijkstras(env, [2, [1, 10]])
 
     # Nice little printy guys
     # for i in env.botList:
@@ -144,8 +155,6 @@ if __name__ == '__main__':
                         i.tCord = -np.pi/2 
                     else:
                         i.tCord = np.pi/2
-                    # print("Bot #:", i.botIndex, "is should be moving at an angle", i.tCord*180/np.pi)
-                    # print("Its goal point is at (", i.path[0].x, ",", i.path[0].y, ")")
                     xDist = i.path[0].x - i.xCord
                     yDist = i.path[0].y - i.yCord
                     if abs(xDist) > 15 or abs(yDist) > 15: 
@@ -156,12 +165,17 @@ if __name__ == '__main__':
                         del(i.path[0]) 
                         
                 tS = time.monotonic_ns()
+                
+            for i in env.botList:
+                if len(i.path) == 0:
+                    t = getCurrLocation(env, i.botIndex)
+                    runDijkstras(env, [i.botIndex, [t.label, 1]])
+                    
             
 
     except KeyboardInterrupt: # If you want to stop the program press crtl + c
-        # cv.destroyAllWindows
         # cv.imshow("Map", env.UIwBots)
-        # cv.waitKey(0)
+        # cv.waitKey()
         print("Aww, you stopped it. Whats wrong with you?")
 
     
--- centralizedCode/Testing.py
+++ centralizedCode/Testing.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import time
 import numpy as np
 import sys
+import csv
 from Classes import environment
 from Classes import partRunner
 from Classes import edge
@@ -9,40 +10,37 @@ from Classes import node
 from Classes import graph
 
 def startUp():
-    nodes = [node(5760, 3240), 
-            node(7000, 3240), 
-            node(11000, 3240), 
-            node(7000, 4500), 
-            node(11000, 4500), 
-            node(9000, 4500), 
-            node(9000, 5500), 
-            node(2500, 5500), 
-            node(2500, 3240),
-            node(2500, 500),
-            node(5760, 500)]
+    fileName = "nodeLocations.csv"
+    nodes = []
+    edges = []
+    rows = []
+    with open(fileName, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for row in csvreader:
+            nodes.append(node(int(row[1]), int(row[2])))
+            for i in row[3:]:
+                if i != '':
+                    edges.append(edge(nodes[int(i)], nodes[int(row[0])]))
+            print()
+        print("Total no of rows: %d"%(csvreader.line_num))
+    
+    for i in edges:
+        print("Node 1: ", i.n1.label, "Node 2: ", i.n2.label)
+    # for i in nodes:
+    #     print("Node: ", i.label, "X: ", i.x, "Y: ", i.y)
+    
 
-    edges = [edge(nodes[0], nodes[1]),
-            edge(nodes[0], nodes[8]),
-            edge(nodes[0], nodes[10]),
-            edge(nodes[1], nodes[2]), 
-            edge(nodes[1], nodes[3]),
-            edge(nodes[2], nodes[4]),
-            edge(nodes[3], nodes[5]),
-            edge(nodes[4], nodes[5]),
-            edge(nodes[5], nodes[6]),
-            edge(nodes[6], nodes[7]),
-            edge(nodes[7], nodes[8]),
-            edge(nodes[8], nodes[9]),
-            edge(nodes[9], nodes[10])]
+    
 
-    g = graph(nodes, edges)
+    # g = graph(nodes, edges)
 
-    b1 = partRunner(nodes[0].x, nodes[0].y, 0)
-    b2 = partRunner(nodes[0].x, nodes[0].y, np.pi/2)
-    b3 = partRunner(nodes[0].x, nodes[0].y, np.pi)
-    bList = np.array([b1, b2, b3])
+    # b1 = partRunner(nodes[0].x, nodes[0].y, 0)
+    # b2 = partRunner(nodes[0].x, nodes[0].y, np.pi/2)
+    # b3 = partRunner(nodes[0].x, nodes[0].y, np.pi)
+    # bList = np.array([b1, b2, b3])
 
-    return bList, g
+    # return bList, g
 
 
 # def paths(g, n1):
@@ -97,16 +95,18 @@ def startUp():
 # for i in previous:
 #     print(previous[i].label)
 
-[bList, g] = startUp()
-for i in g.edges:
-    print(i.label)
-print()
-print(len(g.edges))
-print()
-g.removeEdges([0, 1, 2, 3, 4, 5, 6, 7])
-print()
-for i in g.edges:
-    print(i.label)
-print()
-print(len(g.edges))
-print()
+# [bList, g] = startUp()
+# for i in g.edges:
+#     print(i.label)
+# print()
+# print(len(g.edges))
+# print()
+# g.removeEdges([0, 1, 2, 3, 4, 5, 6, 7])
+# print()
+# for i in g.edges:
+#     print(i.label)
+# print()
+# print(len(g.edges))
+# print()
+
+startUp()
